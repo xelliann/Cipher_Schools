de3c141d1c638229c419146e1d94932f66f2793e
Mohit sharma <mohisharma7890@gmail.com>
2024-06-12 20:49:38 +0530

    commit

---
+++ LEC_7.cpp
@@ -0,0 +1,14 @@
+/* in this lecture we learned about arrays in cpp arrays are used to store multiple values in a single variable name
+to store values in array we need to give array size in the brackets
+Arrays are used to store data efficiently and easily
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int a[10];      //declared an array of size 10 with index value from 0-9
+
+    return 0;
+}
