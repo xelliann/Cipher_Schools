d87850a94ebb25e9ee40ac77449885b69508da4e
Mohit sharma <mohisharma7890@gmail.com>
2024-06-17 14:19:36 +0530

    commit

---
+++ LEC_13.cpp
@@ -0,0 +1,10 @@
+/*
+in this class we discussed about function and what are they used for
+
+Advantage of using function:
+--reuseablity   -->we can use a function again and agian this saves times
+--abstraction   -->hides the useless information from the user
+--divivde and conqure   -->divide the task in small parts and solve our problem
+--improve redability    -->helps to make code more cleaner and easily understandable
+
+*/
