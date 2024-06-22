3e9f37cc9e64ea0e87721ace41d1ba1e9e5e74c2
Mohit sharma <mohisharma7890@gmail.com>
2024-06-22 11:39:06 +0530

    commit

---
+++ LEC_36.cpp
@@ -0,0 +1,9 @@
+/*
+    Algorithm- is the procedure to find the solution of a problem
+    types of algorithms:
+    --searchinng
+    --sorting
+    --recursion
+    --greedy method
+    --two pointers
+*/
