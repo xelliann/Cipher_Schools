ec03958422b0458645f8b1308b47d55cf08267dd
Mohit sharma <mohisharma7890@gmail.com>
2024-06-22 18:21:08 +0530

    commit

--- LEC_39.cpp
+++ LEC_39.cpp
@@ -14,6 +14,25 @@ int main(){
     for(int i = 0;i<n;i++){
         cin>>arr[i];
     }
+
+    //descending order
+    for(int i=0;i<n-1;i++){
+        
+        for(int j=0;j<n-i-1;j++){
+            int temp;
+            if(arr[j]<arr[j+1]){
+                temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    //ascending order
     for(int i=0;i<n-1;i++){
         
         for(int j=0;j<n-i-1;j++){
