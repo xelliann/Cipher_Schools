405094905c91e226b7dfbafe015692b462355b5f
Mohit sharma <mohisharma7890@gmail.com>
2024-06-23 14:23:56 +0530

    commit

---
+++ LEC_44.cpp
@@ -0,0 +1,10 @@
+/* we saw in class that the diffrence between x and x^2 is very large as the size of x increases the time taken
+by the compiler to execute the code will increase a lot that in relation to x
+
+so we need a code with complexity in kx
+
+we need to ignore the constants (they dont matter in large data sets)
+we need to ignore the lower order terms(at larger scale they diminish)
+
+
+*/
