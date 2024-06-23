9a90efa9b0ebb8c7500d5a74565c0f4f3703b138
Mohit sharma <mohisharma7890@gmail.com>
2024-06-23 14:26:11 +0530

    commit

--- LEC_44.cpp
+++ LEC_44.cpp
@@ -6,5 +6,9 @@ so we need a code with complexity in kx
 we need to ignore the constants (they dont matter in large data sets)
 we need to ignore the lower order terms(at larger scale they diminish)
 
+Asymptotic relationship is relation between two equations at infinity and this tends to be a large value
 
+BIG O NOTATION is asymtotic upper bound 
+BIG OMEGA is the aymptotic lower bound 
+BIG THETHA is the middle bound of two values
 */
