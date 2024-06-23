0d0445d3452a025bf462a61b9084c810478fc3aa
Mohit sharma <mohisharma7890@gmail.com>
2024-06-23 11:50:06 +0530

    commit

--- LEC_40.cpp
+++ LEC_40.cpp
@@ -10,6 +10,8 @@ int main(){
     for(int i = 0;i<n;i++){
         cin>>arr[i];
     }
+
+    //ascending order
     int key;
     for(int i = 1;i<n;i++){
         key = arr[i];
@@ -24,5 +26,26 @@ int main(){
     for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+
+    //descending order
+    int arr1[n];
+    cout<<endl<<"Enter array elements again for descending order: "<<endl;
+    for(int i = 0;i<n;i++){
+        cin>>arr1[i];
+    }
+    int key1;
+    for(int i=1;i<n;i++){
+        key1 = arr1[i];
+        int j = i-1;
+        while(arr1[j]<key1 && j>=0){
+            arr1[j+1] = arr1[j];
+            j--;    
+        }
+        arr1[j+1] = key1;
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr1[i]<<" ";
+    }
+ 
     return 0;
 }
